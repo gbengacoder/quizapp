32ff771c536518eb832561506ae6d7459d288dac
peter <petergbenga5@gmail.com>
2023-11-17 18:05:11 +0100

    Initial

---
+++ questions.js
@@ -0,0 +1,520 @@
+const questions = [
+  {
+    question: "How many days makes a week ?",
+    optionA: "10 days",
+    optionB: "14 days",
+    optionC: "5 days",
+    optionD: "7 days",
+    correctOption: "optionD",
+  },
+
+  {
+      question: "30 days has ______ ?",
+    optionA: "January",
+    optionB: "December",
+    optionC: "June",
+    optionD: "August",
+    correctOption: "optionC",
+},
+
+{
+    question: "How manay hours can be found in a day ?",
+    optionA: "30 hours",
+    optionB: "38 hours",
+    optionC: "48 hours",
+    optionD: "24 hours",
+    correctOption: "optionD",
+  },
+
+  {
+      question: "Which is the longest river in the world ?",
+      optionA: "River Nile",
+    optionB: "Long River",
+    optionC: "River Niger",
+    optionD: "Lake Chad",
+    correctOption: "optionA",
+  },
+    {
+      question: "How many players are allowed on a soccer pitch ?",
+      optionA: "10 players",
+      optionB: "11 players",
+      optionC: "9 players",
+      optionD: "12 players",
+      correctOption: "optionB",
+  },
+  {
+      question: "Who was the first President of USA ?",
+        optionA: "Donald Trump",
+      optionB: "Barack Obama",
+      optionC: "Abraham Lincoln",
+      optionD: "George Washington",
+      correctOption: "optionD",
+    },
+
+  
+  {
+    question: "_____ is the hottest Continent on Earth ?",
+    optionA: "Oceania",
+    optionB: "Antarctica",
+    optionC: "Africa",
+    optionD: "North America",
+    correctOption: "optionC",
+  },
+
+  {
+    question: "Which country is the largest in the world ?",
+    optionA: "Russia",
+    optionB: "Canada",
+    optionC: "Africa",
+    optionD: "Egypt",
+    correctOption: "optionA",
+  },
+
+  {
+    question: "Which of these numbers is an odd number ?",
+    optionA: "Ten",
+    optionB: "Twelve",
+    optionC: "Eight",
+    optionD: "Eleven",
+    correctOption: "optionD",
+  },
+
+  {
+    question: `"You Can't see me" is a popular saying by`,
+    optionA: "Eminem",
+    optionB: "Bill Gates",
+    optionC: "Chris Brown",
+    optionD: "John Cena",
+    correctOption: "optionD",
+  },
+
+  {
+    question: "Where is the world tallest building located ?",
+    optionA: "Africa",
+    optionB: "California",
+    optionC: "Dubai",
+    optionD: "Italy",
+    correctOption: "optionC",
+  },
+
+  {
+    question: "The longest river in the United Kingdom is ?",
+    optionA: "River Severn",
+    optionB: "River Mersey",
+    optionC: "River Trent",
+    optionD: "River Tweed",
+    correctOption: "optionA",
+  },
+
+  {
+    question: "How many permanent teeth does a dog have ?",
+    optionA: "38",
+    optionB: "42",
+    optionC: "40",
+    optionD: "36",
+    correctOption: "optionB",
+  },
+
+  {
+    question: "Which national team won the football World cup in 2018 ?",
+    optionA: "England",
+    optionB: "Brazil",
+    optionC: "Germany",
+    optionD: "France",
+    correctOption: "optionD",
+  },
+
+  {
+    question: "Which US state was Donald Trump Born ?",
+    optionA: "New York",
+    optionB: "California",
+    optionC: "New Jersey",
+    optionD: "Los Angeles",
+    correctOption: "optionA",
+  },
+
+  {
+    question: "How man states does Nigeria have ?",
+    optionA: "24",
+    optionB: "30",
+    optionC: "36",
+    optionD: "37",
+    correctOption: "optionC",
+  },
+
+  {
+    question: "____ is the capital of Nigeria ?",
+    optionA: "Abuja",
+    optionB: "Lagos",
+    optionC: "Calabar",
+    optionD: "Kano",
+    correctOption: "optionA",
+  },
+
+  {
+    question: "Los Angeles is also known as ?",
+    optionA: "Angels City",
+    optionB: "Shining city",
+    optionC: "City of Angels",
+    optionD: "Lost Angels",
+    correctOption: "optionC",
+  },
+
+  {
+    question: "What is the capital of Germany ?",
+    optionA: "Georgia",
+    optionB: "Missouri",
+    optionC: "Oklahoma",
+    optionD: "Berlin",
+    correctOption: "optionD",
+  },
+
+  {
+    question: "How many sides does an hexagon have ?",
+    optionA: "Six",
+    optionB: "Sevene",
+    optionC: "Four",
+    optionD: "Five",
+    correctOption: "optionA",
+},
+
+  {
+    question: "How many hearts does an Octopus have ?",
+    optionA: "One",
+    optionB: "Two",
+    optionC: "Three",
+    optionD: "Four",
+    correctOption: "optionC",
+  },
+ 
+  {
+    question: "How many teeth does an adult human have ?",
+    optionA: "28",
+    optionB: "30",
+    optionC: "32",
+    optionD: "36",
+    correctOption: "optionC",
+  },
+  {
+    question: "What is the capital of Japan?",
+    optionA: "Seoul",
+    optionB: "Beijing",
+    optionC: "Tokyo",
+    optionD: "Bangkok",
+    correctOption: "optionC",
+  },
+  {
+    question: "Which element has the chemical symbol 'O'?",
+    optionA: "Oxygen",
+    optionB: "Gold",
+    optionC: "Silver",
+    optionD: "Iron",
+    correctOption: "optionA",
+  },
+  {
+    question: "In which year did World War II end?",
+    optionA: "1943",
+    optionB: "1945",
+    optionC: "1950",
+    optionD: "1939",
+    correctOption: "optionB",
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    optionA: "Atlantic Ocean",
+    optionB: "Indian Ocean",
+    optionC: "Arctic Ocean",
+    optionD: "Pacific Ocean",
+    correctOption: "optionD",
+  },
+  {
+    question: "Who wrote 'Romeo and Juliet'?",
+    optionA: "Charles Dickens",
+    optionB: "William Shakespeare",
+    optionC: "Jane Austen",
+    optionD: "Mark Twain",
+    correctOption: "optionB",
+  },
+  {
+    question: "What is the currency of Brazil?",
+    optionA: "Euro",
+    optionB: "Peso",
+    optionC: "Real",
+    optionD: "Yen",
+    correctOption: "optionC",
+  },
+  {
+    question: "Which planet is known as the 'Morning Star' or 'Evening Star'?",
+    optionA: "Mars",
+    optionB: "Venus",
+    optionC: "Jupiter",
+    optionD: "Saturn",
+    correctOption: "optionB",
+  },
+  {
+    question: "What is the largest desert in the world?",
+    optionA: "Sahara Desert",
+    optionB: "Gobi Desert",
+    optionC: "Antarctica",
+    optionD: "Arabian Desert",
+    correctOption: "optionC",
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    optionA: "Vincent van Gogh",
+    optionB: "Pablo Picasso",
+    optionC: "Leonardo da Vinci",
+    optionD: "Claude Monet",
+    correctOption: "optionC",
+  },
+  {
+    question: "Which gas do plants absorb from the atmosphere?",
+    optionA: "Oxygen",
+    optionB: "Carbon Dioxide",
+    optionC: "Nitrogen",
+    optionD: "Hydrogen",
+    correctOption: "optionB",
+  },
+  {
+    question: "What is the largest bird in the world?",
+    optionA: "Eagle",
+    optionB: "Ostrich",
+    optionC: "Penguin",
+    optionD: "Pelican",
+    correctOption: "optionB",
+  },
+  {
+    question: "Who developed the theory of relativity?",
+    optionA: "Isaac Newton",
+    optionB: "Albert Einstein",
+    optionC: "Galileo Galilei",
+    optionD: "Stephen Hawking",
+    correctOption: "optionB",
+  },
+  {
+    question: "What is the capital of Australia?",
+    optionA: "Sydney",
+    optionB: "Melbourne",
+    optionC: "Canberra",
+    optionD: "Brisbane",
+    correctOption: "optionC",
+  },
+  {
+    question: "In which year did the Titanic sink?",
+    optionA: "1912",
+    optionB: "1920",
+    optionC: "1901",
+    optionD: "1931",
+    correctOption: "optionA",
+  },
+  {
+    question: "Who is known as the 'Father of Computer Science'?",
+    optionA: "Bill Gates",
+    optionB: "Ada Lovelace",
+    optionC: "Alan Turing",
+    optionD: "Steve Jobs",
+    correctOption: "optionC",
+  },
+
+  {
+    question: "What is the speed of light in a vacuum?",
+    optionA: "299,792 kilometers per second",
+    optionB: "150,000 miles per second",
+    optionC: "500,000 kilometers per hour",
+    optionD: "1 million miles per second",
+    correctOption: "optionA",
+  },
+  {
+    question: "What is the smallest prime number?",
+    optionA: "0",
+    optionB: "1",
+    optionC: "2",
+    optionD: "3",
+    correctOption: "optionC",
+  },
+  {
+    question: "Who formulated the laws of motion?",
+    optionA: "Isaac Newton",
+    optionB: "Albert Einstein",
+    optionC: "Galileo Galilei",
+    optionD: "Niels Bohr",
+    correctOption: "optionA",
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    optionA: "Au",
+    optionB: "Ag",
+    optionC: "Fe",
+    optionD: "Cu",
+    correctOption: "optionA",
+  },
+  {
+    question: "In computer science, what does 'AI' stand for?",
+    optionA: "Artificial Intelligence",
+    optionB: "Automated Interaction",
+    optionC: "Advanced Interface",
+    optionD: "Automated Internet",
+    correctOption: "optionA",
+  },
+  {
+    question: "What is the boiling point of water in Fahrenheit?",
+    optionA: "100°F",
+    optionB: "212°F",
+    optionC: "180°F",
+    optionD: "150°F",
+    correctOption: "optionB",
+  },
+  {
+    question: "Who developed the theory of general relativity?",
+    optionA: "Isaac Newton",
+    optionB: "Albert Einstein",
+    optionC: "Stephen Hawking",
+    optionD: "Niels Bohr",
+    correctOption: "optionB",
+  },
+  {
+    question: "What is the chemical symbol for water?",
+    optionA: "H2O",
+    optionB: "CO2",
+    optionC: "O2",
+    optionD: "N2",
+    correctOption: "optionA",
+  },
+  {
+    question: "What is the largest prime number less than 100?",
+    optionA: "89",
+    optionB: "97",
+    optionC: "101",
+    optionD: "79",
+    correctOption: "optionB",
+  },
+  {
+    question: "In physics, what is Planck's constant?",
+    optionA: "6.626 x 10^-34 J·s",
+    optionB: "3.1416",
+    optionC: "9.8 m/s^2",
+    optionD: "2.718",
+    correctOption: "optionA",
+  },
+  {
+    question: "What is the chemical symbol for nitrogen?",
+    optionA: "Ni",
+    optionB: "N",
+    optionC: "Na",
+    optionD: "Ne",
+    correctOption: "optionB",
+  },
+  {
+    question: "What is the capital of South Korea?",
+    optionA: "Seoul",
+    optionB: "Tokyo",
+    optionC: "Beijing",
+    optionD: "Bangkok",
+    correctOption: "optionA",
+  },
+  {
+    question: "Who developed the first successful polio vaccine?",
+    optionA: "Marie Curie",
+    optionB: "Jonas Salk",
+    optionC: "Louis Pasteur",
+    optionD: "Alexander Fleming",
+    correctOption: "optionB",
+  },
+  {
+    question:
+      "In mathematics, what is the value of pi (π) to two decimal places?",
+    optionA: "3.14",
+    optionB: "3.16",
+    optionC: "3.18",
+    optionD: "3.12",
+    correctOption: "optionA",
+  },
+
+  {
+    question: "How many planets are currently in the solar system ?",
+    optionA: "Eleven",
+    optionB: "Seven",
+    optionC: "Nine",
+    optionD: "Eight",
+    correctOption: "optionD",
+  },
+
+  {
+    question: "Which Planet is the hottest ?",
+    optionA: "Jupitar",
+    optionB: "Mercury",
+    optionC: "Earth",
+    optionD: "Venus",
+    correctOption: "optionB",
+  },
+
+  {
+    question: "where is the smallest bone in human body located?",
+    optionA: "Toes",
+    optionB: "Ears",
+    optionC: "Fingers",
+    optionD: "Nose",
+    correctOption: "optionB",
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    optionA: "Earth",
+    optionB: "Mars",
+    optionC: "Jupiter",
+    optionD: "Saturn",
+    correctOption: "optionC",
+  },
+  {
+    question: "What is the speed of sound in air at sea level?",
+    optionA: "343 meters per second",
+    optionB: "1500 meters per second",
+    optionC: "700 meters per second",
+    optionD: "1000 meters per second",
+    correctOption: "optionA",
+  },
+  {
+    question: "Who discovered penicillin?",
+    optionA: "Marie Curie",
+    optionB: "Alexander Fleming",
+    optionC: "Louis Pasteur",
+    optionD: "Jonas Salk",
+    correctOption: "optionB",
+  },
+  {
+    question: "Who is the author of 'The Theory of Everything'?",
+    optionA: "Stephen Hawking",
+    optionB: "Albert Einstein",
+    optionC: "Niels Bohr",
+    optionD: "Isaac Newton",
+    correctOption: "optionA",
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    optionA: "Atlantic Ocean",
+    optionB: "Indian Ocean",
+    optionC: "Arctic Ocean",
+    optionD: "Pacific Ocean",
+    correctOption: "optionD",
+  },
+
+];
+
+function getRandomItems(array, numItems) {
+  // Make a copy of the original array to avoid modifying it
+  const shuffledArray = array.slice();
+
+  // Fisher-Yates shuffle algorithm
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  // Return the first numItems items
+  return shuffledArray.slice(0, numItems);
+}
+
+const shuarray = getRandomItems(questions, 10);
+//console.log(shuarray);
+// const currentQuestion = shuarray[cur];
+
+export default questions;
---
+++ src/App.jsx
@@ -0,0 +1,180 @@
+import { useEffect, useState } from "react";
+import questions from "../questions";
+
+function App() {
+  const [cur, setCur] = useState(Math.floor(Math.random() * questions.length));
+  const [option, setOption] = useState("");
+  const [totalScore, setTotalScore] = useState(0);
+
+  const [modalIsShown, setModalIsShown] = useState(false);
+  const [answered, setAnswered] = useState(0);
+  const [shown, setShown] = useState(1);
+  const [isFinished, setIsFinished] = useState(false);
+  const [allQuestion, setAllQuestion] = useState([]);
+  const [ansModalIsShown, setAnsModalIsShown] = useState(false);
+  const currentQuestion = questions[cur];
+
+  useEffect(() => {
+    checkForLast();
+  }, [cur, totalScore]);
+  const ansHandler = () => {
+    if (option == "") {
+      return;
+    }
+    if (option === currentQuestion.correctOption) {
+      setCur(cur + 1);
+      setTotalScore(totalScore + 1);
+      currentQuestion.chosenAnswer = option;
+    } else {
+      currentQuestion.chosenAnswer = option;
+    }
+    setCur(Math.floor(Math.random() * questions.length));
+
+    setOption("");
+    setShown(shown + 1);
+    setAnswered(answered + 1);
+
+    allQuestion.push(currentQuestion);
+    console.log(allQuestion);
+  };
+
+  const optionsHandler = (ans) => {
+    setOption(ans);
+  };
+
+  const checkForLast = () => {
+    if (shown == 15) {
+      setIsFinished(true);
+      return;
+    }
+  };
+
+  const displayHandler = () => {
+    if (option == "") {
+      return;
+    }
+    if (option === currentQuestion.correctOption) {
+      setTotalScore(totalScore + 1);
+    }
+
+    setModalIsShown(true);
+    setOption("");
+    allQuestion.push(currentQuestion);
+  };
+
+  const init = () => {
+    setCur(Math.floor(Math.random() * questions.length));
+    setIsFinished(false);
+    setModalIsShown(false);
+    setTotalScore(0);
+    setShown(1);
+    setAnswered(0);
+    setAllQuestion([]);
+    setAnsModalIsShown(false);
+  };
+
+  const showAnswersModal = () => {
+    setAnsModalIsShown(true);
+    setModalIsShown(false);
+  };
+
+  return (
+    <div className="mainContainer">
+      <div className="container">
+        {ansModalIsShown && (
+          <div className="ansModal">
+            <div onClick={() => init()} className="x">
+              X
+            </div>
+            {allQuestion.map((q) => (
+              <div className="ansA">
+                <div className="ansQ">
+                  <p>{q.question}</p>
+                </div>
+                <div className="ansAns">
+                  <p className={"optionA" === q.correctOption ? "green" : ""}>
+                    option A: {q.optionA}
+                  </p>
+                  <p className={"optionB" === q.correctOption ? "green" : ""}>
+                    option B: {q.optionB}
+                  </p>
+                  <p className={"optionC" === q.correctOption ? "green" : ""}>
+                    option C: {q.optionC}
+                  </p>
+                  <p className={"optionD" === q.correctOption ? "green" : ""}>
+                    option D {q.optionD}
+                  </p>
+                </div>
+              </div>
+            ))}
+            <div className="btnX">
+              <button onClick={() => init()} className="playAgain">
+                Play Again
+              </button>
+            </div>
+          </div>
+        )}
+
+        {modalIsShown && (
+          <div className="scoreModal">
+            <div className="content">
+              <p>
+                You Scored <b>{totalScore} </b> of a possible <b>{15} </b>
+              </p>
+
+              <button onClick={() => showAnswersModal()}>Show Answers</button>
+              <button onClick={() => init()}>Play Again</button>
+            </div>
+          </div>
+        )}
+        {!modalIsShown && !ansModalIsShown && (
+          <>
+            <div className="qContainer">{currentQuestion.question}</div>
+            <div className="ansContainer">
+              <div
+                onClick={() => optionsHandler("optionA")}
+                className={option == "optionA" ? "selected" : "ans"}
+              >
+                {" "}
+                A:{currentQuestion.optionA}
+              </div>
+              <div
+                onClick={() => optionsHandler("optionB")}
+                className={option == "optionB" ? "selected" : "ans"}
+              >
+                B:{currentQuestion.optionB}
+              </div>
+              <div
+                onClick={() => optionsHandler("optionC")}
+                className={option == "optionC" ? "selected" : "ans"}
+              >
+                C:{currentQuestion.optionC}
+              </div>
+              <div
+                onClick={() => optionsHandler("optionD")}
+                className={option == "optionD" ? "selected" : "ans"}
+              >
+                D:{currentQuestion.optionD}
+              </div>
+            </div>
+            {isFinished ? (
+              <button className="btnNext" onClick={() => displayHandler()}>
+                Display Score
+              </button>
+            ) : (
+              <button
+                className="btnNext"
+                disabled={false}
+                onClick={() => ansHandler()}
+              >
+                Next
+              </button>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
